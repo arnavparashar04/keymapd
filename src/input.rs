11cae05fb64d0aa3020f8b81da2c10161600c35d
Arnav Parashar <arnavparashar408@gmail.com>
2026-09-21 22:55:50 +0530

    feat: add basic epoll setup

--- src/input.rs
+++ src/input.rs
@@ -1,23 +1,25 @@
-use std::path::PathBuf;
+use std::{os::fd::AsRawFd, path::PathBuf};
 use crate::device;
 use evdev::Device;
 pub struct InputMngr{
-    devices: Vec<evdev::Device>
+    devices: Vec<evdev::Device>,
+    epollfd :i32,
 }
-
 impl InputMngr{
     pub fn new(paths: Vec<PathBuf>) -> Result<Self, Box<dyn std::error::Error>>{
        let mut devices = Vec::new();
-       for path in paths{
+       let epollfd = epoll::create(false)?;
+       for (index, path) in paths.iter().enumerate(){
            let device = evdev::Device::open(path)?;
+           epoll::ctl(epollfd, epoll::ControlOptions::EPOLL_CTL_ADD, device.as_raw_fd(), epoll::Event::new(epoll::Events::EPOLLIN, index as u64))?;
            devices.push(device);
        }
-       Ok(Self{devices})
+       Ok(Self{devices, epollfd})
     }
 }
 
 pub fn print_devices(inputmngr: InputMngr){
    for device in inputmngr.devices{
-       println!("{:?}", device);
+       println!("{:?}", device.name().unwrap_or("Unknown"));
    } 
 }
